0869d64f523d8115e576bac92ed042308089c47e
raidallaoua292 <raidallaoua292@gmail.com>
2024-03-04 16:46:04 +0000

    5&6

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -5,7 +5,6 @@ export default function getSanFranciscoDescription() {
     gdp: '$154.2 billion',
     capita: '$178,479',
   };
-  const { income, gdp, capita } = budget;
 
-  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of $${income}. As of 2015, San Francisco proper had a GDP of $${gdp} billion, and a GDP per capita of $${capita},479.`;
+  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of $${budget.income}. As of 2015, San Francisco proper had a GDP of $${budget.gdp} billion, and a GDP per capita of $${budget.capita},479.`;
 }
