8ae88828a500161ee40d7879405190498f37b10f
raidallaoua292 <71762712+raidallaoua292@users.noreply.github.com>
2024-03-06 18:32:38 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import signUpUser from './4-all-reject';
-import uploadPhoto from './5-all-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(
   firstName,
